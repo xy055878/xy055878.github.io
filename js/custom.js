9b3e6a23c7eb9d14b09f1a4319a9f751f58768d5
xy055878 <2486855935.qq.com>
2023-06-01 18:47:41 +0800

    Site updated: 2023-06-01 18:47:40

---
+++ js/custom.js
@@ -0,0 +1,33 @@
+/**
+ * @Author: 糖丸
+ * @Date: 2023-06-01 14:59:35
+ * @LastEditTime: 2023-06-01 16:46:21
+ * @FilePath: \开始》web前端（目前）d:\黄轩宇\hexo + anzhiyu博客\source\js\custom.js
+ * @Tips: 纵使前路黑暗，也有可以照亮你的星光，而你自身就是光!
+ */
+
+
+
+// function copyText(textToCopy) {
+//     // console.log(this.parentNode.previousElementSibling.previousElementSibling.firstElementChild.innerHTML);
+
+//     // const text = document.querySelector('.bber-reply').parentNode.previousElementSibling.previousElementSibling.firstElementChild.innerHTML;
+
+//     // // 选择要复制的文本元素
+//     // var textToCopy = this.parentNode.previousElementSibling.previousElementSibling.firstElementChild;
+//     // console.log(textToCopy);
+
+//     // 创建 range 对象并选择文本
+//     var range = document.createRange();
+//     range.selectNode(textToCopy);
+//     window.getSelection().addRange(range);
+
+//     // 执行复制命令
+//     document.execCommand("copy");
+
+//     // 取消选中文本
+//     window.getSelection().removeAllRanges();
+
+//     // 在控制台输出提示信息
+//     console.log("已复制文本：" + textToCopy.innerHTML);
+// }
